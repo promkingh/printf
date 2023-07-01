d8ce7024a8bae31966887e14d6c984622bd58e8e
Abdelkarim Douadjia <abdelkarim.douadjia@gmail.com>
2023-07-01 01:25:54 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,33 +1,125 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <unistd.h>
+#include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
 
-#define BUFFER 1024
+#define BUFF_SIZE 1024
+#define UNUSED(x) (void)(x)
+
+/* Falges of Main_h for printf_project*/
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+#define F_MINUS 1
+
+#define S_LONG 2
+#define S_SHORT 1
 
 /**
- * struct formats - a struct data type for checker
- * @sym: the pointer to string of symbols
- * @f: the input function
+ * struct fmt - Struct op
+ *
+ * @fmt: struct format and function
+ * @fn: Associated functions to the format
  */
-typedef struct formats
+struct fmt
 {
-	char *sym;
-	int (*f)(va_list);
-} check_t;
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
+
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: struct format and function
+ * @fmt_t: Associated functions to the format
+ */
+typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
-int my_putchar(char c);
-int _putchar(va_list args);
-int _puts(va_list args);
-int _printint(va_list args);
-int _printpercent(__attribute__((unused))va_list args);
-int checker(const char *format, va_list args);
-int (*get_format(const char *format))(va_list);
-char *_revstring(char *s);
-void _printout(char *s);
-int _printbinary(va_list args);
-unsigned int _baselen(unsigned int n, int base);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
-#endif
+
+int print_unsigned(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_hexadecimal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+    int write_number(int is_positive, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+
+    int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+
+
+
+int get_size(const char *format, int *i);
+
+int print_non_printable(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int is_printable(char);  
+
+int print_pointer(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+
+
+int write_unsgnd(int is_negative, int ind,
+char buffer[],
+	int flags, int width, int precision, int size);
+
+int append_hexa_code(char, char[], int);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int handle_write_char(char c, char buffer[],
+	int flags, int width, int precision, int size);
+
+
+int is_digit(char);
+long int convert_size_number(long int num, int size);
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int write_num(int ind, char bff[], int flags, int width, int precision,
+	int length, char padd, char extra_c);
+    int get_precision(const char *format, int *i, va_list list);
+
+
+long int convert_size_unsgnd(unsigned long int num, int size);
+
+int write_pointer(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start);
+
+
+
+
+int print_hexa(va_list types, char map_to[],
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
+
+#endif 
